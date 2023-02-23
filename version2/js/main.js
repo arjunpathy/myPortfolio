5e1bd16baa16d9a500fe1a594e99a1fcf98c7e8c
arjun <nagarjun2695@gmail.com>
2023-02-23 04:09:06 +0100

    Added Version2

---
+++ version2/js/main.js
@@ -0,0 +1,19 @@
+const phrases = ["a Happy","a Peaceful ","a Wonderful ","an Inspirational","a Bright","an Exciting ","a Joyful ","a Blissful","a Delightful ","a Cheerful ","a Thrilled ","a Sunny","a Jolly ","a Calm ","an Energetic ","a Festive","an Electrifying ","a Lovely ","a Perfect ","a Rejuvenating","an Euphoric ","a Glorious ","a Jubilant ","an Apreciative","a Thankful ","a Reliving ","a Relaxing ","a Fascinating","a Beautiful ","an Awesome ","an Enchanting ","a Bewitching","a Mesmerizing ","a Charming ","an Enthralling ","a Charming","a Nice ","a Fine ","an Excellent ","a Splendid","a Pleasant ","a Terrific ","a Grand ","an Amazing","a Best ","a Marvelous ","a Fabulous ","a Lucky","a Super ","a Momentous ","a Magnificent ","an Incredible "];
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+$('#day').text(day);
+
+const el = document.querySelector('.text');
+const fx = new TextScramble(el);
+let counter = 0;
+
+const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 5000);
+    });
+    counter = (counter + 1) % phrases.length;
+};
+
+next();
