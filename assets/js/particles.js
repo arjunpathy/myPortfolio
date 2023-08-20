c47968dbd38bd6d79c87f7e1a0e0c9db32f6d74d
arjun <nagarjun2695@gmail.com>
2023-08-20 14:22:08 +0200

    added paticles to BG

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1506,8 +1506,10 @@ window.particlesJS = function(tag_id, params){
   canvas_el.className = pJS_canvas_class;
 
   /* set size canvas */
-  canvas_el.style.width = "100%";
+  canvas_el.style.width = "99%";
   canvas_el.style.height = "100%";
+  canvas_el.style.zIndex= "-1";
+  canvas_el.style.position= "fixed";
 
   /* append canvas */
   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
